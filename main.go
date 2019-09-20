e8a98d2b2b71681753a23afa91d7daffb494b4ed
Eric Bottard <ebottard@pivotal.io>
2019-09-20 17:24:12 +0200

    Honor the PORT and GRPC_PORT environment variables

--- main.go
+++ main.go
@@ -12,20 +12,23 @@ import (
 )
 
 func main() {
-	grpcAddress := os.Getenv("GRPC_ADDRESS")
-	if grpcAddress == "" {
-		grpcAddress = ":8081"
+	// Lookup the gRPC address where our child process expects to run.
+	grpcPort := os.Getenv("GRPC_PORT")
+	if grpcPort == "" {
+		grpcPort = "8081"
 	}
-	httpAddress := os.Getenv("HTTP_ADDRESS")
-	if httpAddress == "" {
-		httpAddress = ":8080"
+
+	// Per the http-invoker contract, listen for http traffic on a port defined by PORT
+	httpPort := os.Getenv("PORT")
+	if httpPort == "" {
+		httpPort = "8080"
 	}
 	if len(os.Args) < 2 {
 		_, _ = fmt.Fprintf(os.Stderr, "Usage: %s invoker-command [invoker-args]...\n", os.Args[0])
 		os.Exit(1)
 	}
 
-	proxy, err := proxy.NewProxy(grpcAddress, httpAddress)
+	proxy, err := proxy.NewProxy(fmt.Sprintf(":%s", grpcPort), fmt.Sprintf(":%s", httpPort))
 	if err != nil {
 		panic(err)
 	}
@@ -39,24 +42,24 @@ func main() {
 	command.Stdout = os.Stdout
 	command.Stdin = os.Stdin
 	command.Stderr = os.Stderr
+	// The following makes sure that our child process sees the GRPC_PORT variable too.
+	// It should not care about the PORT variable
 	command.Env = os.Environ()
 
 	if err := command.Start(); err != nil {
 		panic(err)
 	}
 
-
 	done := make(chan struct{}, 2)
 
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM)
 	sig := <-stop
-	_=sig
+	_ = sig
 
-	if err := command.Process.Signal(sig) ; err != nil {
+	if err := command.Process.Signal(sig); err != nil {
 		panic(err)
 	}
-
 
 	go func() {
 		_ = command.Wait()
