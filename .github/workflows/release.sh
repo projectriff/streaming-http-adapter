a6813ca902b44f894a54e2127b134bb8da0cb4db
Eric Bottard <ebottard@pivotal.io>
2019-09-23 14:30:08 +0200

    Fix target URL

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -20,4 +20,4 @@ readonly git_timestamp=$(TZ=UTC git show --quiet --date='format-local:%Y%m%d%H%M
 readonly slug=${version}-${git_timestamp}-${GITHUB_SHA:0:16}
 
 echo "Publishing riff http->streaming adapter"
-(cd $root && gsutil cp -a public-read streaming-http-adapter-linux-amd64.tgz gs://projectriff/streaming-http-adapter-buildpack/streaming-http-adapter-linux-amd64-${slug}.tgz)
+(cd $root && gsutil cp -a public-read streaming-http-adapter-linux-amd64.tgz gs://projectriff/streaming-http-adapter/streaming-http-adapter-linux-amd64-${slug}.tgz)
