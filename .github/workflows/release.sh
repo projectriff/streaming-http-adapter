93e76bf57c7be00df387fcd6c312d455f4cc406c
Eric Bottard <ebottard@pivotal.io>
2019-09-23 14:24:57 +0200

    Ditch GCloud action

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -4,6 +4,15 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# from https://cloud.google.com/sdk/docs/downloads-apt-get
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update && sudo apt-get install google-cloud-sdk
+
+gcloud config set disable_prompts True
+gcloud auth activate-service-account --key-file <(echo ${GCLOUD_CLIENT_SECRET} | base64 --decode)
+
 readonly root=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." >/dev/null 2>&1 && pwd)
 readonly version=$(cat ${root}/VERSION)
 readonly git_branch=${GITHUB_REF:11} # drop 'refs/head/' prefix
